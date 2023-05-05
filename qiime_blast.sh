b2fb24f51904c3f6620f9abc4e93ead7e452da32
deannabeatty <60719010+deannabeatty@users.noreply.github.com>
2023-05-05 15:34:32 -0700

    Update: add sequence variant analysis (#2)

--- qiime_blast.sh
+++ qiime_blast.sh
@@ -5,15 +5,25 @@
  source activate qiime2-2020.2
 
  # classify sequences with mitohelper reference database (10.5281/zenodo.6336244)
- # blast classify sequences 0.99
+
+ # blast sequences 0.97 
+ qiime feature-classifier classify-consensus-blast --i-query qza/rep_seqs_fish_2021_dada2.qza --i-reference-reads qza/12S-16S-18S-seqs.qza --i-reference-taxonomy qza/12S-16S-18S-tax.qza --o-classification qza/rep_seqs_fish_2021_dada2_mitofish_blast_taxonomy_12S_16S_18S_97.qza --p-perc-identity 0.97 --p-evalue 0.00001 
+
+ # create qzv of output
+ qiime metadata tabulate \
+ --m-input-file qza/rep_seqs_fish_2021_dada2_mitofish_blast_taxonomy_12S_16S_18S_97.qza \
+ --o-visualization qza/rep_seqs_fish_2021_dada2_mitofish_blast_taxonomy_12S_16S_18S_97.qzv
+
+ # blast OTU clusters
+ # blast 0.99
  qiime feature-classifier classify-consensus-blast --i-query qza/rep_seqs_fish_2021_dada2_de_novo_97.qza --i-reference-reads 12S-16S-18S-seqs.qza --i-reference-taxonomy 12S-16S-18S-tax.qza --o-classification qza/rep_seqs_fish_2021_dada2_de_novo_97_mitofish_blast_taxonomy_12S_16S_18S_99.qza --p-perc-identity 0.99 --p-evalue 0.00001
- # blast classify sequences 0.97
+ # blast 0.97
  qiime feature-classifier classify-consensus-blast --i-query qza/rep_seqs_fish_2021_dada2_de_novo_97.qza --i-reference-reads 12S-16S-18S-seqs.qza --i-reference-taxonomy 12S-16S-18S-tax.qza --o-classification qza/rep_seqs_fish_2021_dada2_de_novo_97_mitofish_blast_taxonomy_12S_16S_18S_97.qza --p-perc-identity 0.97 --p-evalue 0.00001 
- # blast classify sequences 0.95
+ # blast 0.95
  qiime feature-classifier classify-consensus-blast --i-query qza/rep_seqs_fish_2021_dada2_de_novo_97.qza --i-reference-reads 12S-16S-18S-seqs.qza --i-reference-taxonomy 12S-16S-18S-tax.qza --o-classification qza/rep_seqs_fish_2021_dada2_de_novo_97_mitofish_blast_taxonomy_12S_16S_18S_95.qza --p-perc-identity 0.95 --p-evalue 0.00001
- # blast classify sequences 0.90
+ # blast 0.90
  qiime feature-classifier classify-consensus-blast --i-query qza/rep_seqs_fish_2021_dada2_de_novo_97.qza --i-reference-reads 12S-16S-18S-seqs.qza --i-reference-taxonomy 12S-16S-18S-tax.qza --o-classification qza/rep_seqs_fish_2021_dada2_de_novo_97_mitofish_blast_taxonomy_12S_16S_18S_90.qza --p-perc-identity 0.90 --p-evalue 0.00001
- # blast classify sequences 0.80
+ # blast 0.80
  qiime feature-classifier classify-consensus-blast --i-query qza/rep_seqs_fish_2021_dada2_de_novo_97.qza --i-reference-reads 12S-16S-18S-seqs.qza --i-reference-taxonomy 12S-16S-18S-tax.qza --o-classification qza/rep_seqs_fish_2021_dada2_de_novo_97_mitofish_blast_taxonomy_12S_16S_18S_80.qza --p-perc-identity 0.80 --p-evalue 0.00001
 
  # remove sequences that remain unassigned at 0.80 similarity
